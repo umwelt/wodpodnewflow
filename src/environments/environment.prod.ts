859c4dfe2e4fa81e28d3a8f8671a238c8750d450
hardik.lakhani <hardik.lakhani@iflair.com>
2019-02-02 18:50:26 +0530

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,18 +2,18 @@ export const environment = {
     production: true,
     hmr: false,
     firebase: {
-        // apiKey: "AIzaSyBMkH2FRpb-IlLFjP5msJ2zQX0X1JqTCGU",
-        // authDomain: "wodpod-d3fc2.firebaseapp.com",
-        // databaseURL: "https://wodpod-f2086.firebaseio.com/",
-        // projectId: "wodpod-f2086",
-        // storageBucket: "gs://wodpod-f2086.appspot.com",
-        // messagingSenderId: ""
-        apiKey: "AIzaSyDuSNkKNc2xE2bkx9tB56dpTAzhmpdvQm4",
-        authDomain: "wodpod-production.firebaseapp.com",
-        databaseURL: "https://wodpod-production.firebaseio.com",
-        projectId: "wodpod-production",
-        storageBucket: "wodpod-production.appspot.com",
-        messagingSenderId: "990234259320"
+        apiKey: "AIzaSyBMkH2FRpb-IlLFjP5msJ2zQX0X1JqTCGU",
+        authDomain: "wodpod-f2086.firebaseapp.com",
+        databaseURL: "https://wodpod-f2086.firebaseio.com",
+        projectId: "wodpod-f2086",
+        storageBucket: "wodpod-f2086.appspot.com",
+        messagingSenderId: "983124408810"
+        // apiKey: "AIzaSyDuSNkKNc2xE2bkx9tB56dpTAzhmpdvQm4",
+        // authDomain: "wodpod-production.firebaseapp.com",
+        // databaseURL: "https://wodpod-production.firebaseio.com",
+        // projectId: "wodpod-production",
+        // storageBucket: "wodpod-production.appspot.com",
+        // messagingSenderId: "990234259320"
     },
     mailchimp: {
         subscriberUri:"https://wodpod.us7.list-manage.com/subscribe/post",
